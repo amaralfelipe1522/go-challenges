22788e3bf0b9f43603048d357b29a8f6cb1bad0d
amaralfelipe1522 <amaral.felipe@live.com>
2020-04-07 14:51:48 -0300

    Challenge four started. GET method created and returning the height name information.

---
+++ challenge-04/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+)
+
+/*
+In order to successfully complete this challenge, your project will have to:
+
+Create a simple REST API in Go that calls out to the PokeAPI and orders the height of these 5 pokemon:
+
+	ditto
+	charizard
+	weedle
+	mew
+	bulbasaur
+
+The PokeAPI documentation can be found here: https://pokeapi.co/
+*/
+
+//Pokemon armazena as informações de nome e altura consumidas da API
+type Pokemon struct {
+	Name   string `json:"name"`
+	Height int    `json:"height"`
+}
+
+func (pkm *Pokemon) getPokemon(name string, waitgroup *sync.WaitGroup) {
+	url := "https://pokeapi.co/api/v2/pokemon/" + name
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal(responseData, &pkm)
+	waitgroup.Done()
+}
+
+func main() {
+	var pkm Pokemon
+	var pkmList []Pokemon
+
+	var waitgroup sync.WaitGroup
+	waitgroup.Add(1)
+	go pkm.getPokemon("ditto", &waitgroup)
+	waitgroup.Wait()
+	pkmList = append(pkmList, pkm)
+	waitgroup.Add(1)
+	go pkm.getPokemon("mew", &waitgroup)
+	waitgroup.Wait()
+	pkmList = append(pkmList, pkm)
+
+	fmt.Println(pkmList)
+}
