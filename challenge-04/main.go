22cce33fc338157c64c79746865b3228d48f4bcf
amaralfelipe1522 <amaral.felipe@live.com>
2020-04-08 14:04:06 -0300

    Challenge four completed

--- challenge-04/main.go
+++ challenge-04/main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sort"
 	"sync"
 )
 
@@ -23,12 +24,13 @@ Create a simple REST API in Go that calls out to the PokeAPI and orders the heig
 The PokeAPI documentation can be found here: https://pokeapi.co/
 */
 
-//Pokemon armazena as informações de nome e altura consumidas da API
+// Pokemon armazena as informações de nome e altura consumidas da API
 type Pokemon struct {
 	Name   string `json:"name"`
 	Height int    `json:"height"`
 }
 
+// Recupera da API um ou mais pokémons passados por parametro, retornando um slice do tipo []Pokemon
 func (pkm *Pokemon) getPokemon(waitgroup *sync.WaitGroup, name ...string) []Pokemon {
 	var pkmSlice []Pokemon
 	for i := 0; i < len(name); i++ {
@@ -42,8 +44,9 @@ func (pkm *Pokemon) getPokemon(waitgroup *sync.WaitGroup, name ...string) []Poke
 		if err != nil {
 			log.Fatal(err)
 		}
+		// Converte []bytes para JSON e armazena no endereço de pkm
 		json.Unmarshal(responseData, &pkm)
-		fmt.Println(*pkm)
+		//fmt.Println(*pkm)
 		pkmSlice = append(pkmSlice, *pkm)
 	}
 
@@ -51,19 +54,29 @@ func (pkm *Pokemon) getPokemon(waitgroup *sync.WaitGroup, name ...string) []Poke
 	return pkmSlice
 }
 
+// Varre o slice de pokemons ordenando em altura e atualizando o slice
+func byHeight(pkmList []Pokemon) []Pokemon {
+
+	sort.Slice(pkmList, func(i, j int) bool {
+		return pkmList[i].Height < pkmList[j].Height
+	})
+
+	return pkmList
+}
+
 func main() {
 	var pkm Pokemon
 	var pkmList []Pokemon
 	var waitgroup sync.WaitGroup
-
+	//Aguarda todos os waitgroup.Done() encerrarem, garantindo assim que todas as chamadas de API foram encerradas
 	waitgroup.Add(2)
 	go func() {
-		pkmList = pkm.getPokemon(&waitgroup, "ditto", "mew", "pikachu")
-		//pkmList = append()
+		//Armazena o pkmSlice retornado para a a struct pkmList
+		pkmList = pkm.getPokemon(&waitgroup, "ditto", "charizard", "weedle", "mew", "bulbasaur")
 		waitgroup.Done()
-		fmt.Println(pkmList)
 	}()
 	waitgroup.Wait()
-	//começou of FOR concluido
-	fmt.Println(pkmList)
+
+	fmt.Println("Lista de Pokémons:", pkmList)
+	fmt.Println("Pokémons por altura:", byHeight(pkmList))
 }
