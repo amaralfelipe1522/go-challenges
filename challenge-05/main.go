406034eb1c255e6599255ec918f80ce4af514bef
amaralfelipe1522 <amaral.felipe@live.com>
2020-04-12 12:12:12 -0300

    Adding insert function in mysql database

--- challenge-05/main.go
+++ challenge-05/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 /*
 Acceptance Criteria
 In order to successfully complete this challenge, your project will have to:
@@ -7,6 +15,27 @@ In order to successfully complete this challenge, your project will have to:
 Expose a CRUD API that will have an in-memory list of shopping cart items.
 Whenever an endpoint is hit within your API, it will have to use logrus to log that event to a file in JSON format.
 */
-func main() {
 
+func insert(tx *sql.Tx, prod string, prodQtd int) {
+	stmt, _ := tx.Prepare("insert into cart(prod, prod_qtd) values (?,?)")
+
+	_, err := stmt.Exec(prod, prodQtd)
+	if err != nil {
+		tx.Rollback()
+		log.Fatal(err)
+	}
+
+	tx.Commit()
+	fmt.Println("Uma linha inserida.")
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:Project@1522@/store")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	tx, _ := db.Begin()
+	insert(tx, "camisinha", 2)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/amaralfelipe1522/go-challenges/challenge-05
+
+go 1.14
+
+require github.com/go-sql-driver/mysql v1.5.0
