319f05d5cfd050062a43823c5885783dc75c06a7
amaralfelipe1522 <amaral.felipe@live.com>
2020-04-12 11:31:11 -0300

    Challenge five started.

---
+++ challenge-05/main.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+Acceptance Criteria
+In order to successfully complete this challenge, your project will have to:
+
+Expose a CRUD API that will have an in-memory list of shopping cart items.
+Whenever an endpoint is hit within your API, it will have to use logrus to log that event to a file in JSON format.
+*/
+func main() {
+
+}
